ce7d14a8d1cccb8d8093050a92079e08e42db6e4
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2023-02-01 15:11:56 +0700

    Add files via upload

--- app/main.js
+++ app/main.js
@@ -6,24 +6,24 @@ router.get('/nulis', async function (req, res, next) {
   res.render('index', {
     title:
       'Nulis Online - ZaaDev',
-    my_url: "https://nulis-zaadev.vercel.app",
+    my_url: "https://nulis-online.vercel.app",
     desc: "Lagi mager nulis? mending Nulis Online aja, dijamin tangan gak bakalan pegel! aowkaowkokw",
-    author: "ZaaDev"
+    author: "Bambang Tri Raharjo"
   })
 })
 
 router.get('/', function (req, res) {
   res.render('home', {
     title:
-      'Nulis Online - ZaaDev',
-    my_url: "https://nulis-zaadev.vercel.app",
+      'Nulis Online - Bambang Developer',
+    my_url: "https://nulis-online.vercel.app",
     desc: "Lagi mager nulis? mending Nulis Online aja, dijamin tangan gak bakalan pegel! aowkaowkokw",
-    author: "ZaaDev"
+    author: "Bambang Tri Raharjo"
   })
 })
 
 router.get('*', function (req, res) {
   res.render('error')
 })
-// tes
+
 module.exports = router
